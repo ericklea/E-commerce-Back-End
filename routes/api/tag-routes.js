5d285780f5c7cb2df07b99d65723ba92c068d280
L E <erickson.leah.j@gmail.com>
2023-05-31 19:33:03 -0600

    updated tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,7 +43,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try{
     const tagData = await Tag.update(req.body, {
@@ -51,6 +51,14 @@ router.put('/:id', (req, res) => {
         id: req.params.id,
       },
     });
+    if (!tagData){
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  }
+  catch (err){
+    res.status(500).json(err);
   }
 });
 
